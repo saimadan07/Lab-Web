b11cd15b218c073487922a96995a841757861b23
saimadan07 <saimadan2004@gmail.com>
2025-07-23 10:09:56 +0530

    Create firebase.ts
    created a fire base in ts file

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
