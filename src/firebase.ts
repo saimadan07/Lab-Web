73b6a9f8e56aa979ffcda6a819f2bc37c3d17bad
saimadan07 <saimadan2004@gmail.com>
2025-07-24 12:51:14 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,17 @@
-// src/firebase.ts
+// src/firebase/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "your-project-id.firebaseapp.com",
   projectId: "your-project-id",
   storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  messagingSenderId: "xxxxxx",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
