f4ccd435ce91985d0c15f6cad439909734032d40
saimadan07 <saimadan2004@gmail.com>
2025-07-25 12:01:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import ContactForm from "./src/ContactForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Overview from "./pages/Overview";
+import Requests from "./pages/Requests";
+import Returns from "./pages/Returns";
+import Inventory from "./pages/Inventory";
+import History from "./pages/History";
+import Analytics from "./pages/Analytics";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Lab Web</h1>
-      <ContactForm />
-    </div>
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="ml-60 w-full p-6 bg-gray-100 min-h-screen">
+          <Routes>
+            <Route path="/overview" element={<Overview />} />
+            <Route path="/requests" element={<Requests />} />
+            <Route path="/returns" element={<Returns />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/analytics" element={<Analytics />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
