38c3cbaf59a4738401b0ac929a3eb25b8d479d4a
saimadan07 <saimadan2004@gmail.com>
2025-07-28 15:40:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Overview from "./pages/Overview";
-import Requests from "./pages/Requests";
-import Returns from "./pages/Returns";
-import Inventory from "./pages/Inventory";
-import History from "./pages/History";
-import Analytics from "./pages/Analytics";
+import Login from "./pages/Login";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
-function App() {
-  return (
-    <Router>
-      <div className="flex">
-        <Sidebar />
-        <div className="ml-60 w-full p-6 bg-gray-100 min-h-screen">
-          <Routes>
-            <Route path="/overview" element={<Overview />} />
-            <Route path="/requests" element={<Requests />} />
-            <Route path="/returns" element={<Returns />} />
-            <Route path="/inventory" element={<Inventory />} />
-            <Route path="/history" element={<History />} />
-            <Route path="/analytics" element={<Analytics />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-
+<Route path="/login" element={<Login />} />
+<Route path="/overview" element={<ProtectedRoute><Overview /></ProtectedRoute>} />
+<Route path="/requests" element={<ProtectedRoute><Requests /></ProtectedRoute>} />
+// Do the same for other pages...
