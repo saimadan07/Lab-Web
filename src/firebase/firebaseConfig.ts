dbc49b7ac9f227a8e844e96d762927650055345f
saimadan07 <saimadan2004@gmail.com>
2025-07-28 15:37:59 +0530

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,23 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Firebase config (you provided)
 const firebaseConfig = {
   apiKey: "AIzaSyCqlVWuroMnV4Db6dq44iD5kc134L6t2-E",
   authDomain: "lab-57201.firebaseapp.com",
   projectId: "lab-57201",
-  storageBucket: "lab-57201.firebasestorage.app",
+  storageBucket: "lab-57201.appspot.com",
   messagingSenderId: "181981134660",
   appId: "1:181981134660:web:45d82a0bc485867d35e111",
   measurementId: "G-T1YCRF7V1R"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-getAnalytics(app);
-
-export const db = getFirestore(app);
 export const auth = getAuth(app);
-
+export const db = getFirestore(app);
